95c147aad6e204720f3d3b3b0b9740d6800489ba
Amyh97 <amy.hollis@btinternet.com>
2022-07-25 12:37:44 +0000

    add products page and filter by department from home page

--- app.py
+++ app.py
@@ -15,7 +15,7 @@ app = Flask(__name__)
 debug = True  # os.environ.get("DEBUG")
 app.config["MONGO_DBNAME"] = "GoldenShoe"
 # MONGO_URI = os.environ.get("MONGO_URI")
-MONGO_URI = ''
+MONGO_URI = 'mongodb+srv://admin:JqFLrW17ywlNBoUy@goldenshoe.0oena.mongodb.net/?retryWrites=true&w=majority'
 app.config["MONGO_URI"] = MONGO_URI
 db = MongoClient(MONGO_URI)
 mongo = PyMongo(app)
@@ -25,8 +25,17 @@ app.config.update(SECRET_KEY=os.urandom(24))
 @app.route("/")
 def home():
     carousel = db.GoldenShoe.products.find()
-    products = list(carousel)[-5:]
-    return render_template('home.html', products=products)
+    items = list(carousel)[-5:]
+    return render_template('home.html', items=items)
+
+
+@app.route("/products", methods=["GET"])
+def view_products(department=None):
+    query_object = {}
+    if request.args.get("department"):
+        query_object["department"] = request.args.get("department")
+    products = db.GoldenShoe.products.find(query_object)
+    return render_template('products.html', products=products)
 
 
 if __name__ == '__main__':
