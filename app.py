bd19daf21ae4c2d97ba11f71cddce7960434b171
Amyh97 <amy.hollis@btinternet.com>
2022-07-25 14:26:59 +0000

    add product detail page

--- app.py
+++ app.py
@@ -1,18 +1,14 @@
 #!/usr/bin/python
 
 import os
-from flask import Flask, render_template, request, redirect, url_for, flash
-from os import path
+from flask import Flask, render_template, request
 from flask_pymongo import PyMongo
 from bson.objectid import ObjectId
 from pymongo import MongoClient
 
-if path.exists("env.py"):
-    import env
-
 
 app = Flask(__name__)
-debug = True  # os.environ.get("DEBUG")
+debug = 'DEVELOPMENT' in os.environ
 app.config["MONGO_DBNAME"] = "GoldenShoe"
 # MONGO_URI = os.environ.get("MONGO_URI")
 MONGO_URI = 'mongodb+srv://admin:JqFLrW17ywlNBoUy@goldenshoe.0oena.mongodb.net/?retryWrites=true&w=majority'
@@ -30,14 +26,22 @@ def home():
 
 
 @app.route("/products", methods=["GET"])
-def view_products(department=None):
+def view_products(department=None, sale=False):
     query_object = {}
     if request.args.get("department"):
         query_object["department"] = request.args.get("department")
+    elif request.args.get("sale"):
+        query_object["sale"] = sale
     products = db.GoldenShoe.products.find(query_object)
     return render_template('products.html', products=products)
 
 
-if __name__ == '__main__':
-    app.run(host=os.environ.get('IP'), port=int(os.environ.get('PORT'
-            )), debug=True)
+@app.route("/product_detail/<product_id>/")
+def product_detail(product_id):
+    product = db.GoldenShoe.products.find_one({"_id": ObjectId(product_id)})
+    return render_template("product_detail.html", product=product)
+
+
+if __name__ == "__main__":
+    app.run(host=os.environ.get("IP"), port=int(os.environ.get("PORT")),
+            debug=True)
